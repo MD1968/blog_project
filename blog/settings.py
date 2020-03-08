ebbcac1ee4fa098e1c656bab95250ecee73a5597
Mark Dyson <52258439+MD1968@users.noreply.github.com>
2020-03-08 14:34:38 +0000

    allowed hosts updated

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get("C9_HOSTNAME"), '127.0.0.1', 'blog-test-app.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get("C9_HOSTNAME"), '127.0.0.1', 'https://dysonsblog.herokuapp.com/']
 
 
 # Application definition
